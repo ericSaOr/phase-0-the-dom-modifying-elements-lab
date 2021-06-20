ffed89fe62b90a5b851a75b692ce6e142b536e17
Eric Sanchez <ericfsod@gmail.com>
2021-06-20 19:18:38 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+main.remove()
+
+// index.html
+// after index.js is processed
+// has a 'newHeader' variable that points to node 'h1#victory':
+
+const newHeader = document.createElement('h1');
+newHeader.id = ("victory");
+document.body.appendChild(newHeader);
+
+//1) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+const getH1 = document.getElementById('victory');
+getH1.innerHTML = "<p>Eric is the champion!</p>"
